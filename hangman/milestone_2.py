1e1be162c558c5fea2b0db8c29436bc1ff333c1a
mkbr01 <contactmk6@gmail.com>
2023-12-15 10:32:36 +0100

    Refactoring code

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,16 +1,20 @@
 import random
 
-# Get user input for a single letter
-guess = input("Enter a single letter: ")
-
-# Check if the input is a valid single letter
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+def get_valid_single_letter():
+    """Get and validate user input for a single letter."""
+    guess = input("Enter a single letter: ")
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+        return guess
+    else:
+        print("Oops! That is not a valid input.")
+        return None
 
 # List of words
 word_list = ["raspberry", "pineapple", "peach", "mango", "dragonfruit"]
+
+# Get and validate user input
+guess = get_valid_single_letter()
 
 # Randomly select a word from the list
 selected_word = random.choice(word_list)
